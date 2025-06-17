261dea1d127eea1df671c1e0c45ded864fed48ca
helenwps <709369680@qq.com>
2025-06-17 11:35:20 +0800

    规范修改

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-theme get -p=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 -s=a367f2-c8.myshopify.com -t=129180205155
+theme get -p=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 -s=a367f2-c8.myshopify.com -t=135621509219
