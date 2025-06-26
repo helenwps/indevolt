ff31098e3c121d48951013692211d811f6976f24
helenwps <709369680@qq.com>
2025-06-26 17:30:23 +0800

    评价模块

--- update.sh
+++ update.sh
@@ -1,1 +1,4 @@
 theme get -p=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 -s=a367f2-c8.myshopify.com -t=135745044579
+theme configure -p=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 -s=a367f2-c8.myshopify.com -t=135745044579
+#切换id
+shopify theme dev --theme 135745044579 or theme watch --themeid=135745044579
